8c09bd42c3791560650d3cb7e820f86baec86480
Bogdan Samoylenko <samoyienko.bohdan@gmail.com>
2019-05-06 10:44:04 +0300

    add swagger

--- src/main/java/com/spring/springboot/controller/ToDoController.java
+++ src/main/java/com/spring/springboot/controller/ToDoController.java
@@ -2,6 +2,8 @@ package com.spring.springboot.controller;
 
 import com.spring.springboot.entity.ToDo;
 import com.spring.springboot.service.ToDoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+
+@Api(value = "ToDoController", description = "REST ToDo API")
 @RestController
 @RequestMapping("/todo/")
 public class ToDoController {
@@ -18,11 +22,13 @@ public class ToDoController {
     @Autowired
     private ToDoService toDoService;
 
+    @ApiOperation(value = "Create new toDo", response = ToDo.class, tags = "createToDo")
     @RequestMapping(value = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ToDo> createToDo(@RequestBody ToDo todo) {
         return new ResponseEntity<>(toDoService.create(todo), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Get toDo by id", response = ToDo.class, tags = "getToDo")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ToDo> getTodoById(@PathVariable UUID id) {
         ToDo toDo = toDoService.getById(id);
@@ -32,6 +38,7 @@ public class ToDoController {
         return new ResponseEntity<>(toDo, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Delete toDo by id", response = ToDo.class, tags = "removeToDo")
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ToDo> removeToDo(@PathVariable("id") UUID id) {
         ToDo user = toDoService.getById(id);
@@ -42,11 +49,13 @@ public class ToDoController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @ApiOperation(value = "update toDo", response = ToDo.class, tags = "updateToDo")
     @RequestMapping(value = "", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ToDo> updateToDo(@RequestBody ToDo todo) {
         return new ResponseEntity<>(toDoService.update(todo), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "get all toDos", response = List.class, tags = "getAllUsers")
     @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<ToDo>> getAllUsers() {
         List<ToDo> toDos = toDoService.getAll();
--- src/main/java/com/spring/springboot/entity/ToDo.java
+++ src/main/java/com/spring/springboot/entity/ToDo.java
@@ -1,5 +1,6 @@
 package com.spring.springboot.entity;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
@@ -9,8 +10,14 @@ import java.util.UUID;
 @Data
 @Table(value = "todo")
 public class ToDo {
+
+    @ApiModelProperty(notes = "id of toDo",name="id",required=true)
     @PrimaryKey
     private UUID id;
+
+    @ApiModelProperty(notes = "title of toDo",name="title",required=true)
     private String title;
+
+    @ApiModelProperty(notes = "description of toDo",name="description",required=true)
     private String description;
 }
